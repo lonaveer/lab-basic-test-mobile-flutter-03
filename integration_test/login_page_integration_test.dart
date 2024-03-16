e1a17f477099bc9b4f3b3c8f70f6f798b4eade19
lonaveer <lonaveer@gmail.com>
2024-03-17 00:31:34 +0700

    lab03-answer-01

--- integration_test/login_page_integration_test.dart
+++ integration_test/login_page_integration_test.dart
@@ -9,50 +9,50 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('Login and Dashboard Integration Tests', () {
-    // testWidgets('Login Success and Fetch User Data', (WidgetTester tester) async {
-      // // เรียกฟังก์ชัน main ของหน้า Login เพื่อโหลดแอปพลิเคชันขึ้นมาแสดงผล
-      // app.main();
+    testWidgets('Login Success and Fetch User Data', (WidgetTester tester) async {
+      // เรียกฟังก์ชัน main ของหน้า Login เพื่อโหลดแอปพลิเคชันขึ้นมาแสดงผล
+      app.main();
 
-      // // รอให้แอปพลิเคชันเริ่มต้น
-      // await tester.pumpAndSettle();
+      // รอให้แอปพลิเคชันเริ่มต้น
+      await tester.pumpAndSettle();
 
-      // // กรอกข้อมูล 'usertest' ลงใน TextField 'usernameField'
-      // await tester.enterText(xxxxx____1.จงเติมคำตอบที่ถูกต้อง_____xxxxx);
+      // กรอกข้อมูล 'usertest' ลงใน TextField 'usernameField'
+      await tester.enterText(find.byKey(Key('usernameField')), 'usertest');
 
-      // // กรอกข้อมูล 'password123' ลงใน TextField 'passwordField'
-      // xxxxx____2.จงเติมคำตอบที่ถูกต้อง_____xxxxx
+      // กรอกข้อมูล 'password123' ลงใน TextField 'passwordField'
+      await tester.enterText(find.byKey(Key('passwordField')), 'password123');
 
-      // // กดปุ่ม login
-      // xxxxx____3.จงเติมคำตอบที่ถูกต้อง_____xxxxx
+      // กดปุ่ม login
+      await tester.tap(find.byKey(Key('loginButton')));
 
-      // // รอการเปลี่ยนหน้า
-      // await tester.pumpAndSettle();
+      // รอการเปลี่ยนหน้า
+      await tester.pumpAndSettle();
 
-      // // ตรวจสอบว่าหน้า Dashboard ถูกแสดงพร้อมข้อมูลผู้ใช้
-      // xxxxx____4.จงเติมคำตอบที่ถูกต้อง_____xxxxx
-    // });
+      // ตรวจสอบว่าหน้า Dashboard ถูกแสดงพร้อมข้อมูลผู้ใช้
+      expect(find.text('Welcome, Mock User!'), findsOneWidget);
+    });
 
-    // testWidgets('Login Failure Display Alert', (WidgetTester tester) async {
-      // // เรียกฟังก์ชัน main ของหน้า Login เพื่อโหลดแอปพลิเคชันขึ้นมาแสดงผล
-      // xxxxx____5.จงเติมคำตอบที่ถูกต้อง_____xxxxx
+    testWidgets('Login Failure Display Alert', (WidgetTester tester) async {
+      // เรียกฟังก์ชัน main ของหน้า Login เพื่อโหลดแอปพลิเคชันขึ้นมาแสดงผล
+      app.main();
 
-      // // รอให้แอปพลิเคชันเริ่มต้น
-      // xxxxx____6.จงเติมคำตอบที่ถูกต้อง_____xxxxx
+      // รอให้แอปพลิเคชันเริ่มต้น
+      await tester.pumpAndSettle();
 
-      // // กรอกข้อมูล 'wrongUser' ลงใน TextField 'usernameField'
-      // await tester.enterText(xxxxx____7.จงเติมคำตอบที่ถูกต้อง_____xxxxx);
+      // กรอกข้อมูล 'wrongUser' ลงใน TextField 'usernameField'
+      await tester.enterText(find.byKey(Key('usernameField')), 'wrongUser');
 
-      // // กรอกข้อมูล 'wrongPassword' ลงใน TextField 'passwordField'
-      // await tester.enterText(xxxxx____8.จงเติมคำตอบที่ถูกต้อง_____xxxxx);
+      // กรอกข้อมูล 'wrongPassword' ลงใน TextField 'passwordField'
+      await tester.enterText(find.byKey(Key('passwordField')), 'wrongPassword');
 
-      // // กดปุ่ม login
-      // xxxxx____9.จงเติมคำตอบที่ถูกต้อง_____xxxxx(find.byKey(Key('loginButton')));
+      // กดปุ่ม login
+      await tester.tap(find.byKey(Key('loginButton')));
 
-      // // ไม่ต้องรอการเปลี่ยนหน้า เพราะจะแสดงแค่ dialog error , จึงสามารถใช้แค่ pump แทน pumpAndSettle ได้
-      // await tester.pump();
+      // ไม่ต้องรอการเปลี่ยนหน้า เพราะจะแสดงแค่ dialog error , จึงสามารถใช้แค่ pump แทน pumpAndSettle ได้
+      await tester.pump();
 
-      // // ตรวจสอบว่ามีการแสดงข้อความ error 'Login failed'
-      // xxxxx____10.จงเติมคำตอบที่ถูกต้อง_____xxxxx
-    // });
+      // ตรวจสอบว่ามีการแสดงข้อความ error 'Login failed'
+      expect(find.text('Login failed'), findsOneWidget);
+    });
   });
 }
