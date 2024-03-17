050023452834905de8651ab56f59a71a7705d56d
lonaveer <lonaveer@gmail.com>
2024-03-17 18:11:49 +0700

    lab03-question-04v2

--- integration_test/product_page_integration_test.dart
+++ integration_test/product_page_integration_test.dart
@@ -7,30 +7,30 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('Product List Integration Test', () {
-    testWidgets('Test if products are loaded and displayed', (WidgetTester tester) async {
-      // เรียกฟังก์ชัน main ของหน้า Login เพื่อโหลดแอปพลิเคชันขึ้นมาแสดงผล
-      xxxxx____1.จงเติมคำตอบที่ถูกต้อง_____xxxxx
+    // testWidgets('Test if products are loaded and displayed', (WidgetTester tester) async {
+    //   // เรียกฟังก์ชัน main ของหน้า Login เพื่อโหลดแอปพลิเคชันขึ้นมาแสดงผล
+    //   xxxxx____1.จงเติมคำตอบที่ถูกต้อง_____xxxxx
 
-      // รอให้แอปพลิเคชันเริ่มต้น
-      xxxxx____2.จงเติมคำตอบที่ถูกต้อง_____xxxxx
+    //   // รอให้แอปพลิเคชันเริ่มต้น
+    //   xxxxx____2.จงเติมคำตอบที่ถูกต้อง_____xxxxx
 
-      // รอจนกว่า API จะโหลดเสร็จสิ้น
-      final DateTime endTime = xxxxx____3.จงเติมคำตอบที่ถูกต้อง_____xxxxx;
-      while (xxxxx____4.จงเติมคำตอบที่ถูกต้อง_____xxxxx) {
-        xxxxx____5.จงเติมคำตอบที่ถูกต้อง_____xxxxx
-        if (xxxxx____6.จงเติมคำตอบที่ถูกต้อง_____xxxxx) {
-          xxxxx____7.จงเติมคำตอบที่ถูกต้อง_____xxxxx // หากพบข้อมูลที่คาดหวัง , ออกจากลูป
-        } else {
-          // ตรวจสอบว่า CircularProgressIndicator ถูกแสดงขณะโหลดข้อมูล
-          expect(find.byType(CircularProgressIndicator), findsOneWidget);
-        }
-      };
+    //   // รอจนกว่า API จะโหลดเสร็จสิ้น
+    //   final DateTime endTime = xxxxx____3.จงเติมคำตอบที่ถูกต้อง_____xxxxx;
+    //   while (xxxxx____4.จงเติมคำตอบที่ถูกต้อง_____xxxxx) {
+    //     xxxxx____5.จงเติมคำตอบที่ถูกต้อง_____xxxxx
+    //     if (xxxxx____6.จงเติมคำตอบที่ถูกต้อง_____xxxxx) {
+    //       xxxxx____7.จงเติมคำตอบที่ถูกต้อง_____xxxxx // หากพบข้อมูลที่คาดหวัง , ออกจากลูป
+    //     } else {
+    //       // ตรวจสอบว่า CircularProgressIndicator ถูกแสดงขณะโหลดข้อมูล
+    //       expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    //     }
+    //   };
 
-      // ตรวจสอบว่า CircularProgressIndicator หายไปหลังจากโหลดข้อมูลเสร็จสิ้น
-      xxxxx____8.จงเติมคำตอบที่ถูกต้อง_____xxxxx
+    //   // ตรวจสอบว่า CircularProgressIndicator หายไปหลังจากโหลดข้อมูลเสร็จสิ้น
+    //   xxxxx____8.จงเติมคำตอบที่ถูกต้อง_____xxxxx
 
-      // ตรวจสอบว่ามี ListTile ถูกแสดงบนหน้าจอซึ่งหมายความว่าข้อมูลถูกโหลดและแสดงผลเรียบร้อย
-      xxxxx____9.จงเติมคำตอบที่ถูกต้อง_____xxxxx
-    });
+    //   // ตรวจสอบว่ามี ListTile ถูกแสดงบนหน้าจอซึ่งหมายความว่าข้อมูลถูกโหลดและแสดงผลเรียบร้อย
+    //   xxxxx____9.จงเติมคำตอบที่ถูกต้อง_____xxxxx
+    // });
   });
 }
